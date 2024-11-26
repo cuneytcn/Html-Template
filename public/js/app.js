fdf80ff9b9628c3f3e43927227775b9b4ed50daf
cuneytcn <cuneytcn@hotmail.com>
2024-11-26 16:39:08 +0300

    sdsdsdssd

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+// Menu Animation
 const menu = document.getElementById('menu');
 const menuOpen = document.getElementById('menu-open');
 const closeMenu = document.getElementById('menu-close');
@@ -33,3 +34,78 @@ closeMenu.addEventListener('click', () => {
 	});
 	initialState = false;
 });
+
+// Menu Animation
+
+// Form Validation
+
+const form = document.getElementById('form');
+const successMessage = document.getElementById('form-message');
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	let isValid = true;
+
+	// Name
+	const nameInput = document.getElementById('name');
+	const nameInputError = document.getElementById('name-error');
+
+	if (nameInput.value.trim() === '') {
+		nameInputError.classList.remove('hidden');
+		isValid = false;
+	} else {
+		nameInputError.classList.add('hidden');
+		isValid = true;
+	}
+
+	// E-Posta
+	const emailInput = document.getElementById('email');
+	const emailInputError = document.getElementById('email-error');
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	if (!emailRegex.test(emailInput.value.trim())) {
+		emailInputError.classList.remove('hidden');
+		isValid = false;
+	} else {
+		emailInputError.classList.add('hidden');
+	}
+
+	// Phone  Number
+	const phoneInput = document.getElementById('phone');
+	const phoneInputError = document.getElementById('phone-error');
+	if (phoneInput.value.trim() === '') {
+		phoneInputError.classList.remove('hidden');
+		isValid = false;
+	} else {
+		phoneInputError.classList.add('hidden');
+	}
+
+	// If form is valid
+	if (isValid) {
+		form.reset();
+		successMessage.classList.remove('hidden');
+		gsap.fromTo(
+			successMessage,
+			{ opacity: 0, y: -50 },
+			{
+				opacity: 1,
+				y: 0,
+				duration: 0.5,
+				onComplete: () => {
+					setTimeout(() => {
+						gsap.to(successMessage, {
+							opacity: 0,
+							y: -50,
+							duration: 0.5,
+							onComplete: () => {
+								successMessage.classList.add('hidden');
+							},
+						});
+					}, 2000);
+				},
+			},
+		);
+	}
+});
+
+// Form Validation
